7ccb5a2f992516f0b711b7ba398075be7ca46667
Sumit prajapati <sumitprajapati189o@gmail.com>
2026-01-10 15:34:13 +0530

    Create var.tf

---
+++ TERRAFORM/ASG/var.tf
@@ -0,0 +1,23 @@
+variable "image_id" {
+    default = "ami-00ca570c1b6d79f36"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "key_pair" {
+    default = "mumbai-soheb"
+}
+
+variable "subnet_id_1" {
+    default = "subnet-0892ae69504afe630"
+}
+
+variable "subnet_id_2" {
+    default = "subnet-08dfb2d1c18ec688e"
+}
+
+variable "vpc_id" {
+    default = "vpc-0c784a7881cad61b7"
+}
