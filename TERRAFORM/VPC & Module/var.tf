12ef464ffa357bc49f5cee68e420d7891207e25a
Sumit prajapati <sumitprajapati189o@gmail.com>
2026-01-10 15:23:13 +0530

    Create var.tf

---
+++ TERRAFORM/VPC & Module/var.tf
@@ -0,0 +1,15 @@
+variable "image-id" {
+    default = "ami-00f46ccd1cbfb363e"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "my-key" {
+    default = "ore-new"
+}
+
+variable "pub-sub"{
+    default = "10.0.0.0/20"
+}
